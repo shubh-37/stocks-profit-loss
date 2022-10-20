583887ada10581738a876b9e646650c4540d14a1
shubh-37 <shubh.arya37@gmail.com>
2022-10-20 16:23:04 +0530

    feat: color code output and error handling for negative values

--- app.js
+++ app.js
@@ -21,18 +21,27 @@ function clickHandler(){
     var currentPriceInput = currentPrice.value;
     //input validators
     if(initialPriceInput&&quantityInput&&currentPriceInput){
-        //processing the input
+        if(initialPriceInput > 0 && quantityInput > 0 && currentPriceInput > 0){
+                //processing the input
         var [calculateValue, percentageValue] = calculate(initialPriceInput,quantityInput,currentPriceInput);
 
         //processing the input as well as rendering the output
     if(calculateValue > 0){
         output.style.display = "block";
+        output.style.color = "green";
         output.innerText = `Yay you made net profit of ${calculateValue} and your percentage profit is ${percentageValue}% 🥳`
     }else if(calculateValue === 0){
+        output.style.display = "block";
         output.innerText = "No pain no gain and, no gain no pain 🧐";
     }else{
+        output.style.display = "block";
+        output.style.color = "red";
         output.innerText = `Oops, you made net loss of ${calculateValue} and your percentage loss is ${percentageValue}% 😔`
     }
+        }else{
+            //error handler
+            output.innerText = "Please enter positive values to proceed!";
+        }
     }else{
         // error handler
         output.innerText = "Please enter all the inputs to get the result!";
